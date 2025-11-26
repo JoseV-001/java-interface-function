28a286d60e93bb9135639cb18957bfa6d9413867
JoseV-001 <Leaphaz@gmail.com>
2025-11-25 23:25:25 -0300

    Versão 4 - nova implementação com Function

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,6 +5,7 @@ import util.UpperCaseName;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import model.entities.Product;
 
@@ -20,7 +21,9 @@ public class Main {
         list.add(new model.entities.Product("Tablet", 350.00));
         list.add(new model.entities.Product("HD Case", 80.90));
 
-       List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+        Function<Product, String> func = p -> p.getName().toUpperCase();
+
+       List<String> names = list.stream().map(func).collect(Collectors.toList());
        //Operação map só funciona para stream, por isso a necessidade de converter
 
         names.forEach(System.out::println);
