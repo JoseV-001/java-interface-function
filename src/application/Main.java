0ed83f04be798f8242c4ba67793073bd9d5b2c16
JoseV-001 <Leaphaz@gmail.com>
2025-11-25 23:14:00 -0300

    feat: Versão 01 - Implementação via classe concreta

---
+++ src/application/Main.java
@@ -0,0 +1,29 @@
+package application;
+
+import util.UpperCaseName;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        List<model.entities.Product> list = new ArrayList<>();
+
+        list.add(new model.entities.Product("TV", 900.00));
+        list.add(new model.entities.Product("Mouse", 50.00));
+        list.add(new model.entities.Product("Tablet", 350.00));
+        list.add(new model.entities.Product("HD Case", 80.90));
+
+       List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+       //Operação map só funciona para stream, por isso a necessidade de converter
+
+        names.forEach(System.out::println);
+
+
+    }
+}
